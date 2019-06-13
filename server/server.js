da6dd4d50ccb6e4e154674b0256631c2b103a4e5
Mr.Beaver00 <dmitriy.bobro11@gmail.com>
2019-06-13 16:24:57 +0300

    Fix_middleware

--- server/server.js
+++ server/server.js
@@ -3,22 +3,31 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import { ioConnection } from './socket/socket';
-import {tokens, typeAuth, config, currencies}from './config/main';
+import { tokens, typeAuth, config, currencies } from './config/main';
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const {port}  = config.serverDef;
+const { port } = config.serverDef;
 
 // event fired every time a new client connects:
 io.on('connection', ioConnection);
 
 app.use(cors());
 
-app.use((req, res, next) => (tokens.includes(typeAuth+' '+req.headers.authorization)) ?
-    next() : res.send(currencies.nonAutorized));
+app.use((req, res, next) => {
+  let checkAuth=false;
+  tokens.forEach(token => {
+    if (typeAuth + ' ' + token === req.headers.authorization) {
+      checkAuth=!checkAuth;
+    }
+  });
+  (checkAuth)?next():res.send(currencies.nonAutorized);
+});
 
-app.get('/v1/currencies', (res) => res.send(currencies.autorized));
+app.get('/v1/currencies', (req, res) => {
+  res.send(currencies.autorized);
+});
 
 server.listen(port);
 
